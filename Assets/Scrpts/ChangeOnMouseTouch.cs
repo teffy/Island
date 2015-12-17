8f0916f50d01fea404107f039908c8d7e3e8d3b2
Someone love this era ! <lsmfeixiang@163.com>
2015-12-17 21:14:10 +0800

    做了一些代码练习和屋物理质的

---
+++ Assets/Scrpts/ChangeOnMouseTouch.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeOnMouseTouch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        rd = GetComponent<Renderer>();
+        defaultcolor = rd.material.color;
+	}
+    Renderer rd;
+    Color defaultcolor;
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    void OnMouseOver()
+    {
+        rd.material.color = Color.red;
+        transform.Rotate(0, 90 * Time.deltaTime, 0);
+        //transform.Rotate(180 * Time.deltaTime, 90 * Time.deltaTime, 270 * Time.deltaTime);
+    }
+    void OnMouseExit()
+    {
+        rd.material.color = defaultcolor;
+    }
+    public AudioSource music;
+    void OnMouseDown()
+    {
+        if (music.isPlaying)
+        {
+            music.Pause();
+        }
+        else
+        {
+            music.Play();
+        }
+    }
+    void OnMouseUp()
+    {
+        
+    }
+}
---
+++ Assets/Scrpts/MoveUpDown.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveUpDown : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    public float speed = 5.0f;
+    float mspeed;
+    Vector3 v;
+	// Update is called once per frame
+	void Update () {
+        mspeed = speed * 2.0f;
+        Vector3 position = transform.position;
+        print(position.y);
+        if (position.y > 8.0f)
+        {
+            v = new Vector3(0, -mspeed, 0);
+        }
+        else if(position.y <= 0.7f)
+        {
+            v = new Vector3(0, mspeed, 0);
+        }
+        Rigidbody r = GetComponent<Rigidbody>();
+        r.velocity = v;
+	}
+}

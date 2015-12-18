3b6031ef92fb675a0ac1cf0a47f97a518e3ea6da
Someone love this era ! <lsmfeixiang@163.com>
2015-12-18 08:58:22 +0800

    code practice

---
+++ Assets/Scrpts/GotoIsland.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GotoIsland : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UnityEngine.UI.Button gotoIsland = GameObject.Find("Canvas/gotoMyIsland").GetComponent<UnityEngine.UI.Button>();
+        gotoIsland.onClick.AddListener(()=>gotoIslandScene(gameObject));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void gotoIslandScene(GameObject go)
+    {
+        Application.LoadLevel("island");
+    }
+}

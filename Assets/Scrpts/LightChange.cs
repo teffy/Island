559edd5c4d62d84acff9efc93e8a7e267bf92405
Someone love this era ! <lsmfeixiang@163.com>
2015-12-22 08:51:35 +0800

    code practice

--- Assets/Scrpts/LightChange.cs
+++ Assets/Scrpts/LightChange.cs
@@ -3,24 +3,41 @@ using System.Collections;
 
 public class LightChange : MonoBehaviour {
 	Light _light;
-	UnityEngine.UI.Text sum;
-	// Use this for initialization
-	void Start () {
-		_light = gameObject.GetComponent<Light> ();
-		sum = GameObject.Find ("Canvas/sum").GetComponent<UnityEngine.UI.Text> ();
+    GameObject movecube;
+    UnityEngine.UI.Text brightness;
+    // Use this for initialization
+    void Start () {
+        _light = GameObject.Find("Spotlight").GetComponent<Light>();
+        brightness = GameObject.Find ("Canvas/brightness").GetComponent<UnityEngine.UI.Text> ();
+        movecube =GameObject.Find("MoveUpDownCube");
 	}
-	
+    float brightnesschangegap = 0.02f;
+    int textsizechangegap = 1;
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.L)) {
-			_light.intensity += 0.5f;
-			sum.text = "当亮度：" + _light.intensity;
-
+			_light.intensity += brightnesschangegap;
+            brightness.color = Color.red;
+            brightness.fontSize += textsizechangegap;
+            if (brightness.fontSize > 40)
+            {
+                brightness.fontSize = 40;
+            }
+            brightness.text = "当亮度：" + _light.intensity;
 		}
 		if (Input.GetKey (KeyCode.K)) {
-			_light.intensity -= 0.5f;
-			sum.text = "当亮度：" + _light.intensity;
-
+			_light.intensity -= brightnesschangegap;
+            brightness.color = Color.green;
+            brightness.fontSize -= textsizechangegap;
+            if(brightness.fontSize < 12)
+            {
+                brightness.fontSize = 12;
+            }
+            brightness.text = "当亮度：" + _light.intensity;
 		}
-	}
+        if (Input.GetKey(KeyCode.S))
+        {
+            movecube.SendMessage("getMessage");
+        }
+    }
 }
--- Assets/Scrpts/MoveUpDown.cs
+++ Assets/Scrpts/MoveUpDown.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 
 public class MoveUpDown : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
+    UnityEngine.UI.Text sum;
+    // Use this for initialization
+    void Start () {
+        sum = GameObject.Find("Canvas/sum").GetComponent<UnityEngine.UI.Text>();
+    }
     public float speed = 5.0f;
     float mspeed;
     Vector3 v;
@@ -26,4 +26,9 @@ public class MoveUpDown : MonoBehaviour {
         Rigidbody r = GetComponent<Rigidbody>();
         r.velocity = v;
 	}
+    void getMessage()
+    {
+
+        sum.text = v.x + v.y + "";
+    }
 }

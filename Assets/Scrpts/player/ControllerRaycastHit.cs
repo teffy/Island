bcab22e6bde29d0b1c42d4bcbba96a620d17b565
Someone love this era ! <lsmfeixiang@163.com>
2016-01-20 23:39:11 +0800

    update readme,add raycast hit script

---
+++ Assets/Scrpts/player/ControllerRaycastHit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerRaycastHit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position, transform.forward,out hitInfo,3.0f))
+        {
+            if(hitInfo.collider.gameObject.tag == "playerdoor")
+            {
+                GameObject currentdoor = hitInfo.collider.gameObject;
+                print("hit hit ");
+            }
+        }
+	}
+}
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ unity3d学习项目，创建游戏世界，创建一个岛屿
 
 课程来自：
 [这里](http://www.51zxw.net/study.asp?vip=11885058)
-4-6
+4-10
 
 如有错误，请指出，我会及时修改，谢谢。
 
@@ -16,3 +16,4 @@ unity3d学习项目，创建游戏世界，创建一个岛屿
 * 4、雾效果，5.x上和天空盒在一起，在Window->Light->Scene面板中，Fog就是雾的效果，另外我下载的资源中有天空的材质，可以修改一下默认的天空效果
 * 5、碰撞器编辑方法如下图：
 ![edit box collider](/screenshot/edit_box_collider.png)
+* 6、C#中 ref参数是引用，out参数为输出参数。例如：public static bool Raycast(Vector3 origin, Vector3 direction, out RaycastHit hitInfo, float maxDistance);

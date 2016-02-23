c63a6c1a382e0f247f84117832d942ade6918811
Someone love this era ! <lsmfeixiang@163.com>
2016-02-23 08:18:57 +0800

    use script on the house to handle open or close door

---
+++ Assets/Scrpts/player/OnTargetEnter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnTargetEnter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        print("tag:"+other.gameObject.tag);
+        if(other.gameObject.tag == "Player")
+        {
+            gameObject.transform.FindChild("door").SendMessage("doorcheck");
+        }
+    }
+
+}
